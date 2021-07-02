dfae79733ed3ea741b058755f7b12acb7117bbc6
Max Dymond <max.dymond@microsoft.com>
2021-07-02 17:26:24 +0100

    Rudimentary github action to build floki (#168)
    Signed-off-by: Max Dymond <max.dymond@microsoft.com>

--- build.sh
+++ build.sh
@@ -1,25 +1,34 @@
 #!/bin/sh
 
-set -x
+set -ex
 
-LABEL=${TRAVIS_TAG}-${TRAVIS_OS_NAME}
+if [ $OS_NAME = "ubuntu-18.04" ]
+then
+  OS_ID="linux"
+elif [ $OS_NAME = "macos-latest" ]
+then
+  OS_ID="osx"
+fi
+
+TAG=$(tomlq -r '.package.version' Cargo.toml)
+
+LABEL=${TAG}-${OS_ID}
 
 rm -rf target
 echo "Starting release build for ${LABEL}"
 
-if [ ${TRAVIS_OS_NAME} = "linux" ]
+if [ ${OS_ID} = "linux" ]
 then
   echo "Building statically linked linux binary"
   docker run --rm -v $(pwd):/home/rust/src -w /home/rust/src ekidd/rust-musl-builder \
     sh -c 'sudo chown -R rust:rust . && cargo build --release && cp target/x86_64-unknown-linux-musl/release/floki .'
-  sudo chown -R travis:travis .
+  sudo chown -R $(id -u):$(id -g) .
   tar -cvzf floki-${LABEL}.tar.gz floki
 
 else
   echo "Building release binary"
   cargo build --release
   zip -j floki-${LABEL}.zip target/release/floki
-
 fi
 
 echo "Release build complete"
